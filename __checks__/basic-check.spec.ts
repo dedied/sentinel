95ca2378268b80f88c7519cf7bde56c9209f754d
Dave Edie <dedavidedie@gmail.com>
2026-01-25 13:42:48 +0000

    test

--- __checks__/basic-check.spec.ts
+++ __checks__/basic-check.spec.ts
@@ -1,7 +1,9 @@
+// @checklyId 392531c5-05e3-4056-8a01-2a059694869e
+// @checklyName FitTrack Basic Check
+
 import { test, expect } from '@playwright/test';
 
-
-test('FitTrack Pro loads correctly', async ({ page }) => {
+test('Attempt to load the landing page when not connected', async ({ page }) => {
   // Navigate to your deployed site
   await page.goto('https://dedied.github.io/fittrack-pro/');
 
@@ -12,7 +14,7 @@ test('FitTrack Pro loads correctly', async ({ page }) => {
   await expect(page.locator('text=FitTrack')).toBeVisible();
 });
 
-test('Save entry', async ({ page }) => {
+test('Attempt to use the save entry button', async ({ page }) => {
   await page.goto('https://dedied.github.io/fittrack-pro/');
   await page.getByRole('button', { name: 'Continue as Guest' }).click();
   await page.getByRole('button', { name: 'Log Workout' }).click();
